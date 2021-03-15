151614c40bb9ecb556d7e6294d160bbab41db081
Nicolas <npellan@gmail.com>
2021-03-15 17:09:25 +0100

    ✨ fetching and displaying data from API

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,35 @@
 // == Import npm
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // == Import
 import './styles.css';
+import axiosInstance from 'src/api';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <h1>Composant : App</h1>
-  </div>
-);
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+
+  const fetchEmployeesData = async () => {
+    const response = await axiosInstance.get('/');
+    setEmployees(response.data);
+  };
+
+  useEffect(() => {
+    fetchEmployeesData();
+  }, []);
+
+  return (
+    <div className="app">
+      {employees.map((employee) => (
+        <p
+          key={employee.id}
+        >
+          {employee.name}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 // == Export
 export default App;
