d88d4c12201d649cc5fd58b4f3b51d737cde2226
Nicolas <npellan@gmail.com>
2021-03-15 17:36:28 +0100

    💄 updated UI and styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 
 // == Import
-import './styles.css';
+import './styles.scss';
 import axiosInstance from 'src/api';
 
 // == Composant
@@ -20,13 +20,20 @@ const App = () => {
 
   return (
     <div className="app">
-      {employees.map((employee) => (
-        <p
-          key={employee.id}
-        >
-          {employee.name}
-        </p>
-      ))}
+      <h1 className="app__title">My Employees</h1>
+      <div className="employees">
+        {employees.map((employee) => (
+          <div
+            className="employee"
+            key={employee.id}
+          >
+            <h2 className="employee__name">{employee.name}</h2>
+            <p className="employee__job">{employee.job_title}</p>
+            <p className="employee__department">in {employee.department} department</p>
+            <p className="employee__company">@ {employee.company_name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
